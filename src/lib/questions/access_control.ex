c062199a1b8c4a5626eef18d37ecefaed13a9536
Matheus Paiva <matheuspaivavieirasilva@gmail.com>
2024-07-02 20:49:44 -0300

    feat: add access control

---
+++ src/lib/questions/access_control.ex
@@ -0,0 +1,21 @@
+defmodule Questions.AccessControl do
+  @moduledoc "Use this module to check if a user is authorized to perform an action"
+  alias Questions.Accounts.User
+
+  @typedoc "Subjects that can perform actions"
+  @type subject() :: User.t()
+
+  @spec authorize(subject(), atom(), any()) :: :ok | :forbidden
+  def authorize(subject, action, resource \\ nil) do
+    if ok?(subject, action, resource), do: :ok, else: :forbidden
+  end
+
+  defp ok?(%User{} = user, :create_user, _resource),
+    do: user.role == "admin"
+
+  defp ok?(%User{} = user, :assign_role, _resource),
+    do: user.role == "admin"
+
+  defp ok?(%User{} = user, :list_users, _resource),
+    do: user.role in ["admin", "monitor"]
+end
---
+++ src/test/questions/access_control_test.exs
@@ -0,0 +1,27 @@
+defmodule Questions.AccessControlTest do
+  use Questions.DataCase, async: true
+
+  alias Questions.AccessControl
+  alias Questions.Factory
+
+  describe "authorize/2" do
+    test "admins can assign roles" do
+      user = Factory.build(:user, role: "admin")
+      assert :ok = AccessControl.authorize(user, :assign_role)
+    end
+
+    test "admins can create users" do
+      user = Factory.build(:user, role: "admin")
+      assert :ok = AccessControl.authorize(user, :create_user)
+    end
+
+    test "the roles that can list user accounts" do
+      roles = ["admin", "monitor"]
+
+      for role <- roles do
+        user = Factory.build(:user, role: role)
+        assert :ok = AccessControl.authorize(user, :list_users)
+      end
+    end
+  end
+end
