5fde5766197c6d47783c5c819288829b6f022393
Matheus Paiva <matheuspaivavieirasilva@gmail.com>
2024-08-02 12:04:44 -0300

    feat: add question view

---
+++ src/lib/questions_web/views/question_view.ex
@@ -0,0 +1,20 @@
+defmodule QuestionsWeb.QuestionView do
+  use QuestionsWeb, :view
+
+  alias QuestionsWeb.AnswerView
+
+  def render("show.json", %{question: question}) do
+    %{data: render_one(question, __MODULE__, "question.json")}
+  end
+
+  def render("question.json", %{question: question}) do
+    %{
+      id: question.id,
+      title: question.title,
+      description: question.description,
+      category: question.category,
+      status: question.status,
+      answers: render_nested_many(question.answers, AnswerView, "answer.json")
+    }
+  end
+end
