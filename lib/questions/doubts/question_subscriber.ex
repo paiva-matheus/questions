ca370a08865dc4e414e5a9329d63bafd41e4ae53
Matheus Paiva <matheuspaivah2@gmail.com>
2024-08-09 11:05:41 -0300

    feat: add question subscriber create changeset

--- lib/questions/doubts/question_subscriber.ex
+++ lib/questions/doubts/question_subscriber.ex
@@ -5,6 +5,7 @@ defmodule Questions.Doubts.QuestionSubscriber do
   import Ecto.Changeset
 
   alias Questions.Accounts.User
+  alias Questions.Repo
 
   @type t :: %__MODULE__{
           id: Ecto.UUID.t(),
@@ -12,7 +13,7 @@ defmodule Questions.Doubts.QuestionSubscriber do
           inserted_at: NaiveDateTime.t(),
           updated_at: NaiveDateTime.t(),
           user_id: Ecto.UUID.t(),
-          user: User.t() | Ecto.Association.NotLoaded.t(),
+          user: User.t() | Ecto.Association.NotLoaded.t()
         }
 
   @primary_key {:id, :binary_id, autogenerate: true}
@@ -24,4 +25,28 @@ defmodule Questions.Doubts.QuestionSubscriber do
     timestamps()
   end
 
+  @spec create_changeset(t(), map()) :: Ecto.Changeset.t()
+  def create_changeset(%__MODULE__{} = question_subscriber, %{} = attrs) do
+    required_fields = ~w(category user_id)a
+
+    question_subscriber
+    |> cast(attrs, required_fields)
+    |> validate_required(required_fields)
+    |> foreign_key_constraint(:user_id)
+    |> validate_inclusion(:category, ["technology", "engineering", "science", "others"])
+    |> validate_user()
+  end
+
+  defp validate_user(
+         %Ecto.Changeset{
+           data: %{
+             user_id: user_id
+           }
+         } = changeset
+       ) do
+    case Repo.get(User, user_id) do
+      %User{role: "monitor"} -> changeset
+      _ -> add_error(changeset, :user, "Only users with the monitor role can subscriber question")
+    end
+  end
 end
---
+++ test/questions/doubts/question_subscriber_test_.exs
@@ -0,0 +1,68 @@
+defmodule Questions.Doubts.QuestionSubscriberTest do
+  use Questions.DataCase, async: true
+
+  alias Questions.Doubts.QuestionSubscriber
+  alias Questions.Factory
+
+  describe "create_changeset/2" do
+    test "returns valid changeset" do
+      user = Factory.insert(:user, role: "monitor")
+
+      attrs = %{
+        category: "technology",
+        user_id: user.id
+      }
+
+      changeset = QuestionSubscriber.create_changeset(%QuestionSubscriber{}, attrs)
+
+      assert changeset.valid?
+
+      assert changeset.changes == %{
+               category: attrs.category,
+               user_id: attrs.user_id
+             }
+    end
+
+    test "validates required fields" do
+      changeset = QuestionSubscriber.create_changeset(%QuestionSubscriber{}, %{})
+      refute changeset.valid?
+
+      assert errors_on(changeset) == %{
+               category: ["can't be blank"],
+               user_id: ["can't be blank"]
+             }
+    end
+
+    test "validates if category is valid" do
+      user = Factory.insert(:user, role: "monitor")
+
+      attrs = %{
+        user_id: user.id,
+        category: "invalid_category"
+      }
+
+      changeset = QuestionSubscriber.create_changeset(%QuestionSubscriber{}, attrs)
+      refute changeset.valid?
+
+      assert errors_on(changeset) == %{
+               category: ["is invalid"]
+             }
+    end
+
+    test "validates if user is monitor" do
+      user = Factory.insert(:user, role: "student")
+
+      attrs = %{
+        user_id: user.id,
+        category: "technology"
+      }
+
+      changeset = QuestionSubscriber.create_changeset(%QuestionSubscriber{}, attrs)
+      refute changeset.valid?
+
+      assert errors_on(changeset) == %{
+               user: ["Only users with the monitor role can subscriber question"]
+             }
+    end
+  end
+end
